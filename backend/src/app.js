d56877d53bb615e5a5061aad9a6f5c8c3035c750
torco34 <torcoromaarias@gmail.com>
2025-07-31 12:13:03 -0500

    morgan install

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const config = require('./config');
+// const cors = require('cors');
+const morgan = require('morgan');
 const app = express();
 const cliente = require('./modules/clientes/rutas');
+// middleware
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 // configuración de rutas
 app.set('port', config.app.port);
 //rutas
